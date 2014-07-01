28045356232e70ad3aba284fa1f3240fd739b9b2
Michal Malkiewicz <binarnyrycerz@gmail.com>
2014-07-01 19:31:54 +0200

    zmiana na develop

--- TestGit/src/pl/malkiewicz/git/TestGit.java
+++ TestGit/src/pl/malkiewicz/git/TestGit.java
@@ -3,6 +3,6 @@ package pl.malkiewicz.git;
 public class TestGit {
 	
 	public static void main (String[] args) {
-		System.out.println("Test!!");
+		System.out.println("Test!! HAHAHA MAM ZMIANE!!");
 	}
 }
